1404814fd11c48c47879d8d55483fec1f0795f15
unknown <artvoitenk072@gmail.com>
2019-07-02 23:55:26 +0300

    Created getFunction

--- src/services/youtube-service.js
+++ src/services/youtube-service.js
@@ -1,2 +1,17 @@
-class YoutubeService {}
+const axios = require("axios");
+class YoutubeService {
+  _apiSearchBase =
+    "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&key=AIzaSyC8-OMvrXtfXgFmC04WiXXrK9MVkLiRH3Y&q=";
+
+  getSearchedVideos = query => {
+    return axios({
+      method: "get",
+      url: `${this._apiSearchBase}${query}`
+    }).then(result => {
+      const res = JSON.parse(result.request.response);
+      return res.items;
+    });
+  };
+}
+
 export default YoutubeService;
