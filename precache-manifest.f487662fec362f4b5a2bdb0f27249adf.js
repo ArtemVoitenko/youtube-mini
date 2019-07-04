8dd7f14677f0ccd4c0669bde1503ff39c4c2e2ad
unknown <artvoitenk072@gmail.com>
2019-07-05 01:45:01 +0300

    Updates

---
+++ precache-manifest.f487662fec362f4b5a2bdb0f27249adf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31abe66bb82b73cfb470f51293e82aac",
+    "url": "/youtube-mini/index.html"
+  },
+  {
+    "revision": "97fe10cce2c08f8446f9",
+    "url": "/youtube-mini/static/css/2.4fc8fa59.chunk.css"
+  },
+  {
+    "revision": "0399a362b2284457db87",
+    "url": "/youtube-mini/static/css/main.0dbc3613.chunk.css"
+  },
+  {
+    "revision": "97fe10cce2c08f8446f9",
+    "url": "/youtube-mini/static/js/2.9eba36de.chunk.js"
+  },
+  {
+    "revision": "0399a362b2284457db87",
+    "url": "/youtube-mini/static/js/main.1f799f7e.chunk.js"
+  },
+  {
+    "revision": "5cbd471c8a459201204c",
+    "url": "/youtube-mini/static/js/runtime~main.6c235e43.js"
+  },
+  {
+    "revision": "0062c34665a3fc0f2278cd4e955702ec",
+    "url": "/youtube-mini/static/media/OpenSans-Bold.0062c346.ttf"
+  },
+  {
+    "revision": "5a798cdadc7cd321e3f72425b70bface",
+    "url": "/youtube-mini/static/media/OpenSans-Regular.5a798cda.ttf"
+  },
+  {
+    "revision": "ebbcac6c0a11e74c208194f4af6dde15",
+    "url": "/youtube-mini/static/media/sprite.ebbcac6c.svg"
+  }
+]);
