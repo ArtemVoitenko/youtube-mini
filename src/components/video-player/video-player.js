e0eb7ce577d7b4de946e811be41244cda9c4f92a
ArtemVoitenko <artvoitenk072@gmail.com>
2019-07-04 10:44:08 +0300

    Video autoplay

--- src/components/video-player/video-player.js
+++ src/components/video-player/video-player.js
@@ -2,6 +2,11 @@ import React from "react";
 import YouTube from "react-youtube";
 
 const VideoPlayer = ({ videoId }) => {
-  return <YouTube videoId={videoId} />;
+  const opts = {
+    playerVars: {
+      autoplay: 1
+    }
+  };
+  return <YouTube videoId={videoId} opts={opts} />;
 };
 export default VideoPlayer;
